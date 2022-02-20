56ab4f63bb51f1a125e2e455e407ba50fa43a7c6
Annie Johnson <ajohnson@rightpoint.com>
2022-02-20 17:04:48 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,75 @@
+import * as React from 'react'
+import RightArrow from '../public/icons/rightArrow'
 import '../styles/globals.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <SiteNavigation />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
 
 export default MyApp
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Image src="/BTFCapital.png" alt="BTF Capital" width={300} height={89} />
+      <div className="footerNavigation">
+        <Link href="/about">About Us</Link>
+        <Link href="/capital">Capital</Link>
+        <Link href="/partner">Partners</Link>
+        <Link href="/faq">FAQs</Link>
+        <Link href="/terms">Legal</Link>
+      </div>
+      <div className="baseline">
+        <span className="copyright">
+          © 2022 BTFCapital.com. All rights reserved.{' '}
+        </span>
+        <Link
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+          passHref
+        >
+          <span className="logo">
+            <Image src="/BTFLogo.png" alt="BTF Logo" width={60} height={50} />
+          </span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function SiteNavigation() {
+  return (
+    <header className="header">
+      <nav className="navigation">
+        <div>
+          <Link aria-label="Business Loans home page" className="navLogo" href="/" passHref>
+            <Image src="/BTFCapital.png" alt="BTF Logo" width={248} height={71} />
+          </Link>
+        </div>
+        <div style={{ display: 'block' }}>
+          <div className="navLinks">
+            <Link href="/about">About Us</Link>
+            <Link href="/capital">Capital</Link>
+            <Link href="/partner">Partners</Link>
+            <Link href="/faq">FAQs</Link>
+            <Link rel="noopener" target="_self" href="/apply" passHref>
+              <span className="cta">
+                <span className="ctaText">Apply Now</span>
+                <RightArrow />
+              </span>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
---
+++ public/icons/rightArrow.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default class RightArrow extends React.Component<{}, {}>{
+    public render(): JSX.Element {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="29.774px" height="29.773px" viewBox="0 0 29.774 29.773" style={{verticalAlign:"top",padding:"0 5px"}} xmlSpace="preserve">
+                <g>
+                    <path fill="rgba(0, 47, 104, 1)" d="M29.774,14.889L18.026,26.636c-0.586,0.585-1.354,0.878-2.121,0.878c-0.766,0-1.535-0.293-2.121-0.879   c-1.172-1.171-1.172-3.071,0-4.243l4.505-4.505H3c-1.656,0-3-1.343-3-3s1.344-3,3-3h15.286l-4.506-4.506   c-1.172-1.171-1.172-3.071,0-4.242c1.172-1.172,3.071-1.172,4.243,0L29.774,14.889z" />
+                </g>
+            </svg>
+        );
+    }
+}
